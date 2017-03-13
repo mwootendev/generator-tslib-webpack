f8b88a53e0195097ec2c5390a2947e7716ca95af
Michael Wooten <mwooten.dev@gmail.com>
2017-03-13 06:56:57 -0400

    Adding initial test cases for Jasmine project.

--- test/app.js
+++ test/app.js
@@ -4,25 +4,63 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-tslib-webpack:app', function () {
- before(function () {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({
-        name: 'test-package',
-        testingFramework: 'Jasmine'
-      })
-      .toPromise();
-  });
 
-  it('creates files', function () {
-    assert.file([
-      '.editorconfig',
-      '.gitignore',
-      'package.json',
-      'tsconfig.json',
-      'tslint.json',
-      'src/Greeter.ts',
-      'src/Greeter.spec.ts',
-      'src/test-package.ts'
-    ]);
+  describe('Jasmine', function() {
+
+    before(function () {
+        return helpers.run(path.join(__dirname, '../generators/app'))
+          .withPrompts({
+            name: 'test-package',
+            testingFramework: 'Jasmine'
+          })
+          .toPromise();
+      });
+
+      it('creates files', function () {
+        assert.file([
+          '.editorconfig',
+          '.gitignore',
+          'karma.conf.js',
+          'package.json',
+          'tsconfig.json',
+          'tslint.json',
+          'webpack.config.js',
+          'src/greeter.ts',
+          'src/greeter.spec.ts',
+          'src/index.ts',
+          '.vscode/settings.json',
+          '.vscode/tasks.json'
+        ]);    
+      });
+
+      describe('package.json', function() {
+
+        it('properly names the project', function() {
+          assert.jsonFileContent('package.json', {name: 'test-package'});
+        });
+
+        it('is configured for Jasmine', function() {
+          assert.fileContent('package.json', /jasmine/);
+        });
+
+        it('does not reference Mocha or Chai', function() {
+          assert.noFileContent('package.json', /mocha/);
+          assert.noFileContent('package.json', /chai/);
+        });
+
+      });
+
+      describe('karma.conf.js', function() {
+
+        it('is configured for Jasmine', function() {
+          assert.fileContent('karma.conf.js', /karma-jasmine/);
+        });
+
+        it('does not reference Mocha or Chai', function() {
+          assert.noFileContent('karma.conf.js', /mocha/);
+          assert.noFileContent('karma.conf.js', /chai/);
+        });
+
+      });
   });
 });
